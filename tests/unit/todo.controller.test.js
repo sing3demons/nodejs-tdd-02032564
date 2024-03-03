033b6ad824ba0a11b1014a14f51898f1c04339c7
sing3demons <sing3demons@live.com>
2024-03-03 11:51:23 +0700

    Refactor TodoController.createTodo unit tests

--- tests/unit/todo.controller.test.js
+++ tests/unit/todo.controller.test.js
@@ -5,28 +5,35 @@ const httpMocks = require('node-mocks-http')
 const mockBody = require('../mock-data/new-todo.json')
 TodoModel.create = jest.fn()
 
-describe('TodoController.createTodo', () => {
-  let req, res
-  beforeEach(() => {
-    req = httpMocks.createRequest()
-    res = httpMocks.createResponse()
-  })
+let req, res
+beforeEach(() => {
+  req = httpMocks.createRequest()
+  res = httpMocks.createResponse()
+})
 
+describe('TodoController.createTodo', () => {
+  beforeEach(() => {
+    req.body = mockBody
+  })
   it('should have a createTodo function', () => {
     expect(typeof TodoController.createTodo).toBe('function')
   })
 
   it('should call TodoModel.create', async () => {
-    req.body = mockBody
     await TodoController.createTodo(req, res)
     expect(TodoModel.create).toBeCalledWith(mockBody)
   })
 
   it('should return 201 response code', async () => {
-    req.body = mockBody
     await TodoController.createTodo(req, res)
     expect(res.statusCode).toBe(201)
     expect(res._isJSON()).toBeTruthy()
     expect(res._isEndCalled()).toBeTruthy()
+  })
+
+  it('should return json body in response', async () => {
+    TodoModel.create.mockReturnValue(mockBody)
+    await TodoController.createTodo(req, res)
+    expect(res._getJSONData()).toStrictEqual(mockBody)
   })
 })
